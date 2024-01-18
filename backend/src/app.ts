e6e03c4abbc937898f4637f9d5a19b7f9cea2b0b
Kandio16 <kandio1604@gmail.com>
2024-01-18 20:03:38 +0200

    fix: change import

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { PORT, SERVICE_NAME } from '~/config';
+import { PORT, SERVICE_NAME } from './config';
 import { handleErrors } from '~/middlewares/error-handler';
 import { routes } from '~/routes';
 import { connectToDatabase } from './services/db';
